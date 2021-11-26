12ef72d785fc368e800bcd86a8d0ef1f0e270458
BoegyNat <bboegy@gmail.com>
2021-11-26 17:28:09 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    
+    int y ;
+    int o = 0;
+    int e = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> y;
+    while(y != 0) {
+        if(y%2 == 0){
+            e = e + 1;
+        }
+        else{
+            o = o + 1;
+        }
+        cout << "Enter an integer: ";
+        cin >> y;
+    }
+   
+   
+    cout << "#Even numbers = " << e << endl;
+    cout << "#Odd numbers = " << o ;
+    
     return 0;
 }
